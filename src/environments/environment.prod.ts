fdfd9df0e7a8cd8c254d50560d16998b4affb107
bshgkek <backslashhug@gmail.com>
2020-02-21 14:21:52 -0500

    Create environment.prod.ts

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA0TEIAabjOwLJC0R_0_9jfuioqAIGjtvQ",
+    authDomain: "hackathon-2020-2.firebaseapp.com",
+    databaseURL: "https://hackathon-2020-2.firebaseio.com",
+    projectId: "hackathon-2020-2",
+    storageBucket: "hackathon-2020-2.appspot.com",
+    messagingSenderId: "357141935225",
+    appId: "1:357141935225:web:5540c74e52dedfe5b190a8"
+  }
+};
